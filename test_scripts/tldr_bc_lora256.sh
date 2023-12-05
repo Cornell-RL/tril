f84fcf34655703b36c24416c2dec3526b568fe6d
jdchang1 <jchang1730@gmail.com>
2023-12-05 16:40:44 -0500

    fix

--- test_scripts/tldr_bc_lora256.sh
+++ test_scripts/tldr_bc_lora256.sh
@@ -3,5 +3,5 @@ accelerate launch --config_file accelerate_cfgs/deepspeed_config.yaml --num_proc
     task=tldr_supervised \
     alg=bc \
     alg.tldr.policy.args.model_name=EleutherAI/gpt-j-6b \
-    alg.tldr.lora.peft_config.r=256 \
-    alg.tldr.lora.peft_config.lora_alpha=512
+    alg.tldr.policy.lora.peft_config.r=256 \
+    alg.tldr.policy.lora.peft_config.lora_alpha=512
