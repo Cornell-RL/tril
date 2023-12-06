088aa5fd40c365fd180b258945d9d3a89bbbc0b0
kiante brantley <kiante.brantley@gmail.com>
2023-12-06 14:00:25 -0500

    changed the name of args

--- test_scripts/tldr_bc_lora64.sh
+++ test_scripts/tldr_bc_lora64.sh
@@ -3,5 +3,5 @@ accelerate launch --config_file accelerate_cfgs/deepspeed_config.yaml --num_proc
     task=tldr_supervised \
     alg=bc \
     alg.tldr.policy.args.model_name=EleutherAI/gpt-j-6b \
-    alg.tldr.policy.lora.peft_config.r=64 \
-    alg.tldr.policy.lora.peft_config.lora_alpha=128
+    alg.tldr.policy.args.lora.peft_config.r=64 \
+    alg.tldr.policy.args.lora.peft_config.lora_alpha=128
